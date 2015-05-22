360cd1a6267648d3991755bbe2700ff3506b7860
ben wanicur <bwanicur@gmail.com>
2015-05-21 19:42:47 -0700

    crappy code

---
+++ board.rb
@@ -0,0 +1,69 @@
+class Board
+
+  attr_reader :board
+
+  def intitalize
+    @board = [
+      [nil, nil, nil],
+      [nil, nil, nil],
+      [nil, nil, nil]
+    ]
+    @turns = 0
+  end
+
+  def make_turn(x, y, player)
+    unless get_mark(x,y)
+      set_mark(x, y, player.mark)
+      @turns += 1
+    end
+  end
+
+  def game_over?
+    winning_move || @turns == 9
+  end
+
+  def winning_move
+    horizon_win || vertical_win || diagonal_win
+  end
+
+  private
+
+  def horizontal_win(mark)
+    0..2.each do |x|
+      counter = 0
+      0..2.each do |y|
+        break if @board[x][y].nil?
+        break unless @board[x][y] == mark
+        counter += 1
+        return true if counter == 2
+      end
+    end
+    return true
+  end
+
+  def vertical_win(mark)
+    x = 0
+    0..2.each do |num|
+      if @board[x][y].nil?
+        x = 0
+        next
+      end
+      return false unless @board[x][y] == mark
+      x = y + 1
+      return true if x == 2
+    end
+    return true
+  end
+
+  def diagonal_win
+  end
+
+  def set_mark(x, y, mark)
+    @board[x][y] = mark
+  end
+
+  def get_mark(x, y)
+    @board[x][y]
+  end
+
+end
---
+++ player.rb
@@ -0,0 +1,12 @@
+class Player
+
+  :attr_reader :name, :mark
+
+  def initialize(name)
+    @name = name
+    @mark = (name == 'computer') ? 'X' : 'O'
+  end
+
+
+
+end
